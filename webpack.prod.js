8510aa78263ce2369b9f65eea8718882d480b9e4
syagawa <syagawapp@gmail.com>
2020-07-10 22:44:55 +0900

    Add webpack.prod.js

---
+++ webpack.prod.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "production",
+  entry: [
+    "./index.js"
+  ],
+  output: {
+    filename: "outside-events.js",
+    path: path.resolve(__dirname, "./"),
+    library: 'v-outside-events',
+    libraryExport: "default",
+    libraryTarget: "umd",
+    globalObject: "this"
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: "eslint-loader",
+        options: {
+          fix: false,
+          formatter: "stylish" //"codeframe"
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      // Vue: ['vue/dist/vue.esm.js', 'default']
+      Vue: 'vue/dist/vue.esm.js'
+    }),
+  ],
+  resolve: {}
+};
