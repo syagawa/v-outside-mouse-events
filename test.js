e0382fae946d95a19ee6c8d31f3676c75fd9077d
syagawa <syagawapp@gmail.com>
2020-07-11 23:36:27 +0900

    Add test-import.html

---
+++ test.js
@@ -0,0 +1,59 @@
+import directive from "./directive.js";
+import Vue from "vue";
+
+
+new Vue({
+  el: "#app",
+  data: {
+    outsides: []
+  },
+  computed: {
+    message: function(){
+      if(this.outsides.length > 0){
+        return "Click outside !!";
+      }
+    }
+  },
+  methods: {
+    click: function(num){
+      this.outsides.splice(0, this.outsides.length);
+      console.log("Click" , num);
+    },
+    clickOutside1: function(){
+      var str = "Click outside 1 !!";
+      this.outsides.push(str);
+      console.log(str);
+    },
+    clickOutside2: function(){
+      var str = "Click outside 2 !!";
+      this.outsides.push(str);
+      console.log(str);
+    },
+    clickOutside3: function(){
+      var str = "Click outside 3 !!";
+      this.outsides.push(str);
+      console.log(str);
+    },
+    clickOutside: function(num){
+      var str = "Click outside " + num + "!!!!!!";
+      console.log(str);
+      this.outsides.push(str);
+    },
+    setClickOutside: function(num){
+      var self = this;
+      return function(){
+        self.clickOutside(num);
+      }
+    }
+  },
+  mounted: function(){
+    console.info("mounted");
+  },
+  created: function(){
+    console.info("created");
+  },
+
+  directives: {
+    "outside-click": directive
+  }
+});
---
+++ webpack.test-import.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "production",
+  entry: [
+    "./test.js"
+  ],
+  output: {
+    filename: "test-import.js",
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: "eslint-loader",
+        options: {
+          fix: false,
+          formatter: "stylish" //"codeframe"
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      // Vue: ['vue/dist/vue.esm.js', 'default']
+      Vue: 'vue/dist/vue.esm.js'
+    }),
+  ],
+  resolve: {}
+};
