238febead4b58b6e4b0344b569df69478e1eeaad
syagawa <syagawapp@gmail.com>
2021-02-17 19:05:15 +0900

    Fix html for test

--- src/test.js
+++ src/test.js
@@ -6,6 +6,7 @@ new Vue({
   el: "#app",
   data: {
     outsides: [],
+    insides: [],
     show: true,
     default_event: "click",
     options: [
@@ -24,33 +25,59 @@ new Vue({
   methods: {
     change: function(){
       this.outsides = [];
+      this.insides = [];
     },
     click: function(num){
       console.log("click");
+      var str = "click inside";
+      this.insides.push(str);
     },
     mousedown: function(num){
+      var str = "mousedown inside";
+      this.insides.push(str);
       console.log("mousedown");
     },
     mouseup: function(num){
+      var str = "mouseup inside";
+      this.insides.push(str);
       console.log("mouseup");
     },
     mouseover: function(num){
+      var str = "mouseover inside";
+      this.insides.push(str);
       console.log("mouseover");
     },
     mousemove: function(num){
+      var str = "mousemove inside";
+      this.insides.push(str);
+
       console.log("mousemove");
     },
     mouseleave: function(num){
+      var str = "mouseleave inside";
+      this.insides.push(str);
+
       console.log("mouseleave");
     },
     mouseout: function(num){
+      var str = "mouseout inside";
+      this.insides.push(str);
       console.log("mouseout");
     },
     mouseenter: function(num){
+      var str = "mouseenter inside";
+      this.insides.push(str);
       console.log("mouseenter");
     },
     touchstart: function(num){
+      var str = "touchstart inside";
+      this.insides.push(str);
       console.log("touchstart");
+    },
+    touchend: function(num){
+      var str = "touchend inside";
+      this.insides.push(str);
+      console.log("touchend");
     },
     clickOutside: function(num){
       var str = "Click outside !!";
